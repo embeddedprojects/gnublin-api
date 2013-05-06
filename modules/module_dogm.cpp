ae3263fe7859577399bc50e6ab1e6d17992b5e29
Felix Steinberger <mail@mycho.eu>
2013-05-06 11:05:27 +0200

    dogm for raspberrypi

--- modules/module_dogm.cpp
+++ modules/module_dogm.cpp
@@ -6,10 +6,14 @@
 
 
 //********** Constructor ***************************
-// set RS-PIN to 14
+// set RS-PIN to 14 / for RPi to 4
 // set RS-PIN as OUTPUT
 gnublin_module_dogm::gnublin_module_dogm(){
+#ifdef BOARD_RASPBERRYPI
+	rs_pin=4;
+#else
 	rs_pin = 14;
+#endif
 	gpio.pinMode(rs_pin, OUTPUT);
 	init_flag = false;
 	error_flag = false;	
