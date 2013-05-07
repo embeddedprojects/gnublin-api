606b0e1cf361bf22b780dda825b34687853f2c3b
manliebert <man.liebert@gmail.com>
2013-05-07 17:13:19 +0200

    changed comments for doxygen

--- modules/module_relay.cpp
+++ modules/module_relay.cpp
@@ -7,56 +7,107 @@
 
 
 //------------------Konstruktor------------------
-// set Port 0 to OUTPUT and LOW
-// set Error Flag flase
-// set standard i2c Address 0x20
-
+/** @~english 
+* @brief Set standards.
+*
+* Set Port 0 to OUTPUT, set standard i2c address 0x20, set ErrorFlag false
+* @~german 
+* @brief Setze Standartwerte.
+*
+* Setze Port 0 als Ausgang, setze standart i2c Adresse 0x20 und setze das ErrorFlag auf false.
+*/
 gnublin_module_relay::gnublin_module_relay() {
 	error_flag=false;
 	pca9555.setAddress(0x20);
 }
 
 
-//-------------get Error Message-------------
-// get the last ErrorMessage
-// parameters:		NONE
-// return:		[const char*]ErrorMessage	Error Message as c-string
-
+//-------------getErrorMessage-------------
+/** @~english 
+* @brief Get the last Error Message.
+*
+* This Funktion returns the last Error Message, which occurred in that Class.
+* @return ErrorMessage as c-string
+*
+* @~german 
+* @brief Gibt die letzte Error Nachricht zurück.
+*
+* Diese Funktion gibt die Letzte Error Nachricht zurück, welche in dieser Klasse gespeichert wurde.
+* @return ErrorMessage als c-string
+*/
 const char *gnublin_module_relay::getErrorMessage(){
 	return ErrorMessage.c_str();
 }
 
-//-------------------------------Fail-------------------------------
-//returns the error flag. if something went wrong, the flag is true
+//-------------------------------fail-------------------------------
+/** @~english 
+* @brief Returns the error flag. 
+*
+* If something went wrong, the flag is true.
+* @return bool error_flag
+*
+* @~german 
+* @brief Gibt das Error Flag zurück.
+*
+* Falls das Error Flag in der Klasse gesetzt wurde, wird true zurück gegeben, anderenfalls false.
+* @return bool error_flag
+*/
 bool gnublin_module_relay::fail(){
 	return error_flag;
 }
 
-//-------------set Address-------------
-// set the slave address
-// parameters:		[int]Address	i2c slave Address
-// return:			NONE
-
+//-------------setAddress-------------
+/** @~english 
+* @brief Set the slave address 
+*
+* With this function you can set the individual I2C Slave-Address of the module.
+* @param Address new I2C slave Address
+*
+* @~german 
+* @brief Setzt Slave Adresse.
+*
+* Mit dieser Funktion kann die individuelle I2C Slave-Adresse des Moduls gesetzt werden.
+* @param Address neue I2C slave Adresse
+*/
 void gnublin_module_relay::setAddress(int Address){
 	pca9555.setAddress(Address);
 }
 
 
-//-------------------set devicefile----------------
-// set the i2c device file. default is "/dev/i2c-1"
-// parameters:		[string]filename	path to the dev file
-// return:			NONE
-
+//-------------setDevicefile-------------
+/** @~english 
+* @brief Set devicefile.
+*
+* With this function you can change the I2C device file. Default is "/dev/i2c-1"
+* @param filename path to the I2C device file
+*
+* @~german 
+* @brief Setzt Device Datei.
+*
+* Mit dieser Funktion kann die I2C Gerätedatei geändert werden. Standartmäßig wird "/dev/i2c-1" benutzt.
+* @param filename Pfad zur I2C Gerätedatei
+*/
 void gnublin_module_relay::setDevicefile(std::string filename){
 	pca9555.setDevicefile(filename);
 }
 
 //-------------------switch Pin----------------
-// switches 1 relay pin, error_flag is set on failure
-// parameters:		[int]pin	Pin 1-8
-//					[int]value	HIGH or LOW
-// return:			[int]1		success
-//					[int]-1		failure
+/** @~english 
+* @brief Switch pin.
+*
+* This function sets the given relay to the given value.
+* @param pin Number of the relay to switch (1-8)
+* @param value close (1) or open (0) the relay (ON, OFF)
+* @return success: 1, failure: -1
+*
+* @~german 
+* @brief Schalte Pin.
+*
+* Schalten der einzelnen Relays. 
+* @param pin Nummer des anzusteuernden Relays (1-8)
+* @param value schließen (1) oder öffnen (0) des Relays (ON, OFF)
+* @return Erfolg: 1, Fehler: -1
+*/
 int gnublin_module_relay::switchPin(int pin, int value) {
 	error_flag=false;
 
