f88bd0085accd519cfb73d2ee72ddf456d8a4136
Julian <sarcher@embedded-projects.net>
2013-09-26 10:27:12 +0200

    Added comments to examples module_dac.cpp

--- examples/module_dac.cpp
+++ examples/module_dac.cpp
@@ -3,27 +3,24 @@
 int main(){
 	gnublin_module_dac dac;
 	
-	/*
-	dac.write(0, 1024);
-	dac.write(1, 1024);
-	dac.write(2, 1024);
-	dac.write(3, 1024);
+	// Write to all to specif channels	
+	dac.writeEeprom(0, 4095); //Writes value to register and EEPROM
+	dac.writeEeprom(1, 2048);
+	dac.write(2, 1024);				//Writes value only to register
+	dac.write(3, 0);
 
-	dac.vRefEeprom(0, 1);
-	dac.vRefEeprom(1, 1);
-	dac.vRef(2, 1);
+	dac.vRefEeprom(0, 1);			//Writes vRef condition also to EEPROM
+	dac.vRefEeprom(1, 1);			
+	dac.vRef(2, 1);						//Channel two will get internal Reference. 0 would be external
 	dac.vRef(3, 1);
-	dac.gainEeprom(0, 1);
+	dac.gainEeprom(0, 1);			//Writes gain condition also to EEPROM
 	dac.gainEeprom(1, 0);
-	dac.gain(2, 1);
-	dac.gain(3, 0);
-	*/
-
-	printf("\n%i\n%i\n%i\n%i\n",dac.read(0), dac.read(1), dac.read(2), dac.read(3));
-	//dac.writeAll(0, 1000, 1500, 2000);
+	dac.gain(2, 1);						//2x Gain. Voltage Reference doubled. Max is External voltage
+	dac.gain(3, 0);						//1x Gain. Voltage Reference is vRef.
 	
-	dac.read(0);
-	dac.read(1);
-	printf("%i", dac.read(2));
-	dac.read(3);
+	//Reads all Channels:
+	printf("\n%i\n%i\n%i\n%i\n",dac.read(0), dac.read(1), dac.read(2), dac.read(3));
+	
+	//Uncomment this for writing to all channels at once
+	//dac.writeAll(0, 1000, 1500, 2000);
 }
